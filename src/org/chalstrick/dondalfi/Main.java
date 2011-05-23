10314c2da2d9e8b55c74b71c573243433f0b6c82
Christian Halstrick <christian.halstrick@sap.com>
2011-05-23 17:40:00 +0200

    create Main class

---
+++ src/org/chalstrick/dondalfi/Main.java
@@ -0,0 +1,11 @@
+package org.chalstrick.dondalfi;
+
+public class Main {
+
+  public static void main(String args[]) {
+    System.out.println(Main.class.getSimpleName()
+        + " was called with the following params:");
+    for (String p : args)
+      System.out.println("param: " + p);
+  }
+}
